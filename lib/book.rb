feffb933e4d1ce03c2d392010ae99880391f0793
Alex Manzi <alex.manzi22@gmail.com>
2022-03-15 11:44:08 -0400

    commit tree e779a9b90c8f248bdadcad3552e64e1bfbaf92be

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    attr_accessor :author, :title, :genre, :page_count
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
